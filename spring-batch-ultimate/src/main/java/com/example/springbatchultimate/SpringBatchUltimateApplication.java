f49fe1f527eee37c0d58b8415dbfd1acce43eb21
henok keraga <henokkeraga@gmail.com>
2021-05-31 15:17:54 -0600

    spring-batch-ultimate

---
+++ spring-batch-ultimate/src/main/java/com/example/springbatchultimate/SpringBatchUltimateApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbatchultimate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBatchUltimateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBatchUltimateApplication.class, args);
+    }
+
+}
---
+++ spring-batch-ultimate/src/test/java/com/example/springbatchultimate/SpringBatchUltimateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbatchultimate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBatchUltimateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
